14368f4a21b45cd54c137ada781e20a5c92eed31
takapi86 <happyturn86b@gmail.com>
2024-05-20 10:25:54 +0900

    インデントを修正

--- src/MagicQuotesGpcEmulator.php
+++ src/MagicQuotesGpcEmulator.php
@@ -16,31 +16,31 @@ class MagicQuotesGpcEmulator {
     define('MagicQuotesGpcEmulatorApplied', true);
   }
 
-    private function addslashesRecursive(&$value)
-    {
-        if (is_array($value)) {
-            foreach ($value as $key => &$item) {
-                $this->addslashesRecursive($item);
-            }
-        } elseif (is_object($value)) {
-            $value = get_object_vars($value);
-            $this->addslashesRecursive($value);
-        } else {
-            $value = addslashes($value);
+  private function addslashesRecursive(&$value)
+  {
+    if (is_array($value)) {
+        foreach ($value as $key => &$item) {
+            $this->addslashesRecursive($item);
         }
+    } elseif (is_object($value)) {
+        $value = get_object_vars($value);
+        $this->addslashesRecursive($value);
+    } else {
+        $value = addslashes($value);
     }
+  }
 
-    public function isApplied() {
-        return defined('MagicQuotesGpcEmulatorApplied');
-    }
+  public function isApplied() {
+    return defined('MagicQuotesGpcEmulatorApplied');
+  }
 
-    public function isMagicQuotesGpcEnabled() {
-        if ($this->isApplied()) {
-            return true;
-        }
-        if (!function_exists('get_magic_quotes_gpc')) {
-            return false;
-        }
-        return get_magic_quotes_gpc();
+  public function isMagicQuotesGpcEnabled() {
+    if ($this->isApplied()) {
+      return true;
     }
+    if (!function_exists('get_magic_quotes_gpc')) {
+      return false;
+    }
+    return get_magic_quotes_gpc();
+  }
 }
